079ad677aae2f20e1b17c987713eb174de5ad808
ahquan421 <minhquan04112005@gmail.com>
2025-06-12 10:49:10 +0700

    masach

--- projectwnc/database/migrations/2025_05_27_041429_create_course.php
+++ projectwnc/database/migrations/2025_05_27_041429_create_course.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('masach');
             $table->string('tensach');
             $table->string('tacgia');
             $table->string('nxb');
